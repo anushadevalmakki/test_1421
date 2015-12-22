6746df46d0f287d1189c9caa5688b541625d9687
akshatashetti <akshatshetti@gmail.com>
2015-12-22 11:37:26 +0530

    commit6

--- Queue/src/Queue/QueueClient.java
+++ Queue/src/Queue/QueueClient.java
@@ -4,12 +4,14 @@ public class QueueClient {
 
 	public static void main(String[] args) {
 		System.out.println("****Stacl Implementation using arrays****");
-		ArrayQueue arrayQueue=new ArrayQueue();
+		//ArrayQueue arrayQueue=new ArrayQueue();
+		ArrayListQueue arrayQueue=new ArrayListQueue();
 		arrayQueue.enquee(1);
 		arrayQueue.display();
 		arrayQueue.enquee(13);
 		arrayQueue.display();
 		arrayQueue.enquee(16);
+		System.out.println("\nsize of elements--> \t:"+arrayQueue.noOFelements());
 		arrayQueue.display();
 		arrayQueue.dequee();
 		arrayQueue.display();
