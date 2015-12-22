b3506c137abb004966ec3568f45f5af13b55f983
akshatashetti <akshatshetti@gmail.com>
2015-12-22 10:47:55 +0530

    commit2

--- Queue/src/Queue/ArrayQueue.java
+++ Queue/src/Queue/ArrayQueue.java
@@ -1,11 +1,72 @@
 package Queue;
-import java.util.ArrayList;
 public class ArrayQueue {
-	static int size;
-	int top,front,rear;
-	ArrayList queue = new ArrayList(); 
+	int size;
+	int head,tail;
+	int queue[];
+	//ArrayList queue = new ArrayList(); 
 	public ArrayQueue(int size) {
-		this.top=-1;
+		this.size=size;
+		this.head=this.tail=-1;
+		this.queue=new int[size];
+	}
+	public void enquee(int value) {
+		if(IsFull())
+			System.out.println("\nThe queue is full");
+		else
+		{
+			if(head==-1)
+			{
+				this.head=head+1;
+				this.tail=tail+1;
+				
+			}else
+			{
+				this.tail=tail+1;
+			}
+			this.queue[tail]=value;
+		}
+	}
+	private boolean IsFull() {
+		if(tail==(size-1))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
+	public void display() {
+		int i;
+		if(IsEmpty())
+			System.out.println("\nThe queue is empty");
+		else
+		{
+			System.out.print("\nThe queue are\t:");
+			for(i=head;i<=tail;i++)
+			System.out.print("  "+queue[i]);
+		}
+		System.out.println("\n");
+	}
+	public void dequee() {
+		if(IsEmpty())
+			System.out.println("\nThe queue is empty");
+		else
+		{
+			System.out.println("Dequee element is\t" +queue[head]);
+			this.head=head+1;
+		}
+		
+	}
+	private boolean IsEmpty() {
+		if(head==size)
+		{
+			return true;
+		}
+			
+		else
+			return false;
 	}
 
 }
--- Queue/src/Queue/QueueClient.java
+++ Queue/src/Queue/QueueClient.java
@@ -3,9 +3,20 @@ package Queue;
 public class QueueClient {
 
 	public static void main(String[] args) {
-		ArrayQueue arrayQueue=new ArrayQueue(3);
-		
-
+		System.out.println("****Stacl Implementation****");
+		ArrayQueue arrayQueue=new ArrayQueue(2);
+		arrayQueue.enquee(1);
+		arrayQueue.display();
+		arrayQueue.enquee(13);
+		arrayQueue.display();
+		arrayQueue.enquee(16);
+		arrayQueue.display();
+		arrayQueue.dequee();
+		arrayQueue.display();
+		arrayQueue.dequee();
+		arrayQueue.display();
+		arrayQueue.dequee();
+	
 	}
 	
 
