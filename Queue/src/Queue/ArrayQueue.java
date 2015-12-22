404e987219b8b46c638d4979efc7b8f7ae832279
anushadevalmakki <anushadevalmakkikar@gmail.com>
2015-12-22 10:00:29 +0530

    commit1

---
+++ Queue/src/Queue/ArrayQueue.java
@@ -0,0 +1,11 @@
+package Queue;
+import java.util.ArrayList;
+public class ArrayQueue {
+	static int size;
+	int top,front,rear;
+	ArrayList queue = new ArrayList(); 
+	public ArrayQueue(int size) {
+		this.top=-1;
+	}
+
+}
---
+++ Queue/src/Queue/QueueClient.java
@@ -0,0 +1,12 @@
+package Queue;
+
+public class QueueClient {
+
+	public static void main(String[] args) {
+		ArrayQueue arrayQueue=new ArrayQueue(3);
+		
+
+	}
+	
+
+}
